3cf9069177a9238b2f0b6fa5edc69db168eac298
Jan Snasel <snaselj@gmail.com>
2015-04-09 18:53:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Open Smart Environment - DVB streamer example
+This package is a part of the OSE suite.
+All packages can be found [on GitHub](https://github.com/opensmartenvironment/).
+
+<b>Open Smart Environment software is a suite for creating
+multi-instance applications that work as a single whole.</b><br>
+Imagine, for example, a personal mesh running on various devices
+including HTPCs, phones, tablets, workstations, servers, Raspberry
+Pis, home automation gadgets, wearables, drones, etc.
+
+OSE software consists of several npm packages: a [framework](http://opensmartenvironment.github.io/doc/#framework running
+on Node.js, an [HTML5 frontend](http://opensmartenvironment.github.io/doc/#html5frontend, extending
+packages and a set of example applications.
+
+<figure>
+  <a href="http://opensmartenvironment.github.io/doc/resource/ose.svg"><img width=100% src="http://opensmartenvironment.github.io/doc/resource/ose.svg"></a>
+
+  <figcaption><b>Set-up of current example applications.</b> Here,
+  OSE provides a [Media player](http://opensmartenvironment.github.io/doc/#example-player) running on an HTPC
+  that can be controlled by an IR remote through
+  [LIRC](http://opensmartenvironment.github.io/doc/#example-lirc) and is capable of playing streams from a
+  [DVB streamer](http://opensmartenvironment.github.io/doc/#example-dvb) and control devices through GPIO
+  pins on a [Raspberry Pi](http://opensmartenvironment.github.io/doc/#example-rpi) </figcaption> </figure>
+
+For more information about OSE see **the [documentation](http://opensmartenvironment.github.io/doc/)**.
+
+## Status
+- Pre-alpha stage (insecure and buggy)
+- Unstable API
+- Patchy documentation
+- No test suite
+
+This is not yet a piece of download-and-use software. It is important
+to understand the basic principles covered by the
+[documentation](http://opensmartenvironment.github.io/doc/).
+
+## Platforms
+OSE has the following prerequisites:
+- Node.js (>0.10) running on Debian Jessie and Raspbian
+- Firefox 37 or newer with Web Components enabled
+
+## Package description
+This example provides an DVB streamer to be used together with an
+[media player example](http://opensmartenvironment.github.io/doc/#mediaplayerexample instance. In this example, a new entry
+controlling a dvblast is created.
+
+The documentation for "ose-example-dvb" package can be found **[here](http://opensmartenvironment.github.io/doc/#ose-example-dvb#)**.
+
+## Licence
+This software is released under the terms of the [GNU General
+Public Licence v3.0](http://www.gnu.org/copyleft/gpl.html) or
+later.
---
+++ bin/run.js
@@ -0,0 +1,151 @@
+#!/usr/bin/env node
+
+/**
+ * @caption DVB streamer example
+ *
+ * @readme
+ * This example provides an DVB streamer to be used together with an
+ * [media player example] instance. In this example, a new entry
+ * controlling a dvblast is created.
+ *
+ * @oseExample 
+ *
+ * @description
+ *
+ * ## Installation
+ *
+ * For the DVB streamer application to work, you need the following
+ * prerequisities: 
+ * - Node.js and npm 
+ * - dvblast
+ *
+ * If you run Debian Jessie, just run:
+ *
+ *     sudo apt-get install dvblast
+ *
+ *
+ * Start the DVB streamer example as follows:
+ *
+ *     cd ose-example-dvb
+ *     ./bin/run.js
+ *
+ * @module example-dvb
+ * @main example-dvb
+ */
+
+/**
+ * @caption DVB streamer example startup script
+ *
+ * @class example-dvb.bin.run
+ * @type module
+ */
+
+
+'use strict';
+
+// The OSE framework is initialized by requiring the "ose" package:
+var O = require('ose').app(module, 'example');
+
+var Remote = require('ose-control/lib/remote');
+var Media = O.class('ose-media/lib/remote');
+
+/*!
+ * OSE is configured by a configuration object, `module.exports` in
+ * this case. Each property of this object defines the configuration
+ * for one [OSE plugin].
+ */
+
+// Basic properties of OSE instance
+exports.ose = {
+  name: 'dvb',          // Name of this OSE instance
+  space: 'example.org',  // Space name this instance belongs to
+};
+
+
+// Enable general control package
+exports['ose-control'] = {};
+
+// Enable general DVB package
+exports['ose-dvb'] = {};
+
+// Enable general media player package
+exports['ose-media'] = {};
+
+// Enable VideoLAN control package
+exports['ose-videolan'] = {};
+
+
+// Enable CLI interface
+exports.cli = {
+  id: 'ose/lib/cli',
+
+  // CLI can run some commands:
+  /*
+  script: [
+    'wait 10000',
+    'space example.org',
+    'shard dvb',
+    'entry dvblast',
+    'info',
+    'detail',
+  ],
+  */
+};
+
+
+// Definition of data structure – The space named "example.org"
+// contains all your data
+exports.space = {
+  id: 'ose/lib/space',         // Module id
+  name: 'example.org',         // Name of the space
+  home: 'media',               // Home instance of the space
+
+  // Peers to connect to
+  peers: {
+    // Media player OSE instance – When running the dvb and player
+    // OSE instances on different machines, change the following IP
+    // address to that of the media player instance.
+    media: 'ws://10.166.25.8:4431',
+  }
+};
+
+// The space is partitioned into shards:
+// DVB shard
+exports.control = {
+  id: 'ose/lib/shard',
+  sid: 5,                   // Shard id unique within the space
+  scope: 'control',         // Scope the shard belongs to
+  alias: 'dvb',            // Shard alias
+  home: 'dvb',             // Home instance of the space
+  entries: initDvb,         // Method initializing entries belonging
+                            // to the shard, defined below
+};
+
+
+/*
+ * @caption Example LIRC remote controller configuration
+ *
+ * @readme
+ * Personalize this file depending on your remote controller and LIRC
+ * setup.
+ *
+ * @class example-lirc.lirc
+ * @type module
+ */
+
+// "lirc" shard initialization method.
+function initDvb(shard) {
+  shard.entry('dvbstreamer', 'dvblast', {
+    name: 'DVBlast',
+
+    /* Uncomment following "mcast" property to enable multicast streaming
+    mcast: {
+      id: 'mcastPool',
+      alias: 'mediaControl',
+    },
+    */
+  });
+}
+
+// Start OSE instance
+O.run();
