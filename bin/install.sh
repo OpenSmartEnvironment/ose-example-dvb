0dd1a2526383fe257e1747736f928e54a3bb7254
Jan Snasel <snaselj@gmail.com>
2015-04-22 12:06:51 +0200

    Dependencies as git repos

--- bin/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-npm install git+https://github.com/OpenSmartEnvironment/ose.git
-npm install git+https://github.com/OpenSmartEnvironment/ose-control.git
-npm install git+https://github.com/OpenSmartEnvironment/ose-dvb.git
-npm install git+https://github.com/OpenSmartEnvironment/ose-media.git
-npm install git+https://github.com/OpenSmartEnvironment/ose-videolan.git
