d523feb55c2fbfa3c66f78ba05071b356212527c
Jan Snasel <snaselj@gmail.com>
2015-11-21 15:32:56 +0100

    Remove: `O.app()`

--- bin/run.js
+++ bin/run.js
@@ -53,7 +53,9 @@
 'use strict';
 
 // The OSE framework is initialized by requiring the "ose" package:
-var O = require('ose').app(module, 'example');
+var O = require('ose').module(module);
+O.scope = 'control';
+O.package = 'ose-example-dvb';
 
 /*!
  * OSE is configured by a configuration object, `module.exports` in
