c57c6ab74b769e020838a65056f537038b1fcc13
Jan Snasel <snaselj@gmail.com>
2016-02-13 14:52:35 +0000

    Doc

--- bin/run.js
+++ bin/run.js
@@ -112,7 +112,7 @@ exports.space = {
   peers: {
     // Media player OSE instance – Change the following IP
     // address to that of the media player instance.
-    player: 'ws://10.166.25.8:4431',  // CHANGE ME !!!!!
+    player: 'ws://10.166.26.2:4431',  // CHANGE_ME !!!!!
   }
 };
 
